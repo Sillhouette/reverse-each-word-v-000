10a191355f4d9fa41f9d8ccdd9e6dbaac2dbbccd
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 23:51:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
+##
+# => #reverse_each_word takes in a string, reverses the letters in each word then returns the string of words in the same order
+##
 def reverse_each_word(string)
   string_array = string.split(" ");
-  reverse_string_array = string_array.collect { |string| string.reverse}
-  return reverse_string_array.join(" ")
+  reverse_string_array = string_array.collect { |string| string.reverse};
+  return reverse_string_array.join(" ");
 end
 
 ##
