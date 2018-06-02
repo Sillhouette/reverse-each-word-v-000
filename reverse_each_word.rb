465c88e97cc541ee6b367588a2a50db44f55d9f3
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 23:50:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split(" ");
-  reverse_string_array = [];
-  string_array.each { |string| reverse_string_array.push(string.reverse)}
+  reverse_string_array = string_array.collect { |string| string.reverse}
   return reverse_string_array.join(" ")
 end
 
